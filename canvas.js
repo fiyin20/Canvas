5653f4d8bc6bf914b5662d84f47850c5c03f57cb
Taiwo Peace Fiyinfoluwa <fiyinfoluwat606@gmail.com>
2024-12-07 12:42:05 +0100

    Updated canvas.js

--- canvas.js
+++ canvas.js
@@ -46,3 +46,9 @@ canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     [lastX, lastY ] = [e.offsetX, e.offsetY];
 });
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
+
+    
+
