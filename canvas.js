a470972bfb0007ff41ce474584f2bd3e992dd350
Taiwo Peace Fiyinfoluwa <fiyinfoluwat606@gmail.com>
2024-12-07 12:31:14 +0100

    Updated canvas.js

--- canvas.js
+++ canvas.js
@@ -1,1 +1,13 @@
+ 
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADASS';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
+ctx.globalCompositionOperation = 'mutiply';
 
+let isDrawing = false;
+let lastX = 0;
